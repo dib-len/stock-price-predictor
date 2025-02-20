a0c1fd36ab2623dc3df77c14b21e27388e504ada
Sinan <sinan.iqbal@focusrite.com>
2025-02-20 14:04:07 +0000

    Update data retrieving for Random Forest

--- trading-site/main.py
+++ trading-site/main.py
@@ -1,10 +1,19 @@
 import yfinance as yf
 import pandas as pd
 
-def get_stock_data(ticker):
+def get_stock_data(ticker, period="1y"):
     stock_data = yf.Ticker(ticker)
-    stock_data = stock_data.history(period="30d")
-    stock_data = stock_data[['Close']]
-    return stock_data
+    df = stock_data.history(period=period)
 
-print(get_stock_data('AAPL'))
+    df['Prev Close'] = df['Close'].shift(1)
+    df['High-low'] = df['High'] - df['Low']
+    df['Open-close'] = df['Open'] - df['Close']
+    df['Volume'] = df['Volume']
+    df['MA10'] = df['Close'].rolling(window=10).mean()
+
+    df['Next Close Prediction'] = df['Close'].shift(-1)
+
+    df.dropna(inplace=True)
+    return df
+
+print(get_stock_data("AAPL"))
