90ba625571ce6c5dcb9b8b1a62f718b382ddeb66
Sinan <sinan.iqbal@focusrite.com>
2025-02-20 16:15:32 +0000

    Implement basic Svelte frontend for retrieving predicted prices

--- trading-site/src/routes/+page.svelte
+++ trading-site/src/routes/+page.svelte
@@ -1,1 +1,17 @@
-<h1>Hello!</h1>
+<script>
+    let predictedPrice = $state("");
+    let ticker = $state("");
+
+    async function fetchPredictedPrice() {
+        const response = await fetch(`http://localhost:8000/predict/${ticker}`);
+        const data = await response.json();
+        predictedPrice = data.predicted_price;
+    }
+    
+</script>
+
+<h1>Hello! {predictedPrice}</h1>
+<input type="text" bind:value={ticker} placeholder="Enter"/>
+<button onclick={fetchPredictedPrice}>
+    Predict
+</button>
